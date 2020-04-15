4425c703f5c58b372a39ee6e80a867eecc3d729f
Colin Brayton <gringolalia@gmail.com>
2020-04-15 19:12:33 -0300

    deploy

---
+++ precache-manifest.82b0201b8f9abad96f0cdc182cfe2751.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc00ee4814a8a9e65d42",
+    "url": "/css/about.211e5b37.css"
+  },
+  {
+    "revision": "f4134b7dd6803ea49644",
+    "url": "/css/app.c3ea71f9.css"
+  },
+  {
+    "revision": "6ae45b54b2d59b4003cd",
+    "url": "/css/chunk-vendors.3c76955e.css"
+  },
+  {
+    "revision": "a575704df9edcca0e69685620db2df3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc00ee4814a8a9e65d42",
+    "url": "/js/about.ab538537.js"
+  },
+  {
+    "revision": "f4134b7dd6803ea49644",
+    "url": "/js/app.90c95b99.js"
+  },
+  {
+    "revision": "6ae45b54b2d59b4003cd",
+    "url": "/js/chunk-vendors.84bdf6d2.js"
+  },
+  {
+    "revision": "9241b752529a771b6992f87a87ed398e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
